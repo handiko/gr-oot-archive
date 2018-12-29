29c1a35020896c4352ca0468ce1cd037a74dbc43
handiko <handikogesang@gmail.com>
2018-12-29 16:22:54 +0700

    Recompile

--- gr-display/build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/handiko/gr-oot-archive/gr-display/apps
-# Build directory: /home/handiko/gr-oot-archive/gr-display/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- gr-display/build/include/display/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/handiko/gr-oot-archive/gr-display/include/display
-# Build directory: /home/handiko/gr-oot-archive/gr-display/build/include/display
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
